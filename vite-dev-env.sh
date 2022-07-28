63f02c084824b0050576ff71851cdbfc5fee5046
bo7owers <rene7orres@gmail.com>
2022-07-28 15:36:45 -0400

    initial commit

---
+++ vite-dev-env.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Choose a vite-based project, open project in your editor of choice, start dev env and open browser
+
+projectLocations=~/Documents/websites/vite-apps
+editorOfChoice=codium
+
+projectActions="$(cd $projectLocations/ && ls -A)"
+
+select project in ${projectActions}; do cd $projectLocations/${project} && editorOfChoice $projectLocations/${project}; break;
+done
+
+# This works on Linux only
+xdg-open http://localhost:3000/ 
+npm run dev
+
+
+
